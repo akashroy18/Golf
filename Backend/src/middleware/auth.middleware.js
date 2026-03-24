a6fcc44c426d40e9b6af6a991fa2120969932675
akashroy18 <akash02052005@gmail.com>
2026-03-24 21:44:59 +0530

    Auth middleware Done

---
+++ Backend/src/middleware/auth.middleware.js
@@ -0,0 +1,21 @@
+import User from '../models/user.model.js'
+import jwt from 'jsonwebtoken'
+export const auth_middleware = async (req,res,next)=>{
+    try {
+        const token = req.cookies.user_token
+        if(!token){
+            return res.status(400).json({
+                message:"unauthorized"
+            })
+        }
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        const user = await userModel.findById(decoded.id).select("-password")
+        if (!user) {
+        return res.status(401).json({ message: "User not found" })
+        }
+        req.user=user
+        next()
+    } catch (error) {
+        return res.status(401).json({ message: "Invalid or expired token" })
+    }
+}
